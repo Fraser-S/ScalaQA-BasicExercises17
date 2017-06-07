6d2dc9ae8fa6afedb9cade979011843900f80fb7
Fraser <frasers@hotmail.co.uk>
2017-06-07 09:35:47 +0000

    minor change to logic with nothing new, added comments so that it is easier to see what is going on

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,13 @@ import java.util.TimeZone
 
 object Main {
   def main(args : Array[String]) : Unit = {
-    println(TimeZone.getAvailableIDs.filter(_.size > 1).map(t => t.split('/') last).grouped(10).map(t => t(0)).mkString(", "));
+    println( //print the array
+      TimeZone.getAvailableIDs.//get time zone id's
+      filter(_.length>1). //filter out all the size 1 arrays
+      map(t => t.split('/') last). //map the results and split('/')last removes everything before the '/' of each element
+      grouped(10). //group results into groups of 10's
+      map(t => t(0)). //map the first element in each group
+      mkString(", ") //needed for displaying
+    )//end of print statement
   }
 }
